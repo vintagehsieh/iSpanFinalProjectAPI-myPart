33a9e2f0b5259e34a6d11353f8f89080d376b0af
vintagehsieh <vintagehsieh@gmail.com>
2023-04-14 01:08:51 +0800

    iSpanFinalProject - Hsiang’s Part

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,73 @@
+﻿using api.iSMusic.Models.Infrastructures.Extensions;
+using api.iSMusic.Models.Services;
+using api.iSMusic.Models.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.iSMusic.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class MembersController : ControllerBase
+	{
+		private readonly IMemberRepository _memberRepository;
+
+		
+		private readonly MemberService _memberService;
+
+
+        public MembersController(IMemberRepository memberRepo,IActivityRepository activityRepository)
+		{
+			_memberRepository = memberRepo;
+		
+            _memberService = new(_memberRepository, activityRepository);			
+        }
+
+
+		[HttpGet]
+		[Route("Activities")]
+		public IActionResult GetMemberFollowedActivities()
+		{
+            int memberId = this.GetMemberId();
+            var result = _memberService.GetMemberFollowedActivities(memberId);
+			if (!result.Success)
+			{
+				return BadRequest(result.Message);
+			}
+
+			return Ok(result.Dtos.Select(dto => dto.ToIndexVM()));
+
+        }
+
+		[HttpPost]
+		[Route("Activities/{activityId}/{attendDate}")]
+		public IActionResult FollowActivity([FromRoute] int activityId, [FromRoute] DateTime attendDate)
+		{
+			
+			int memberId = this.GetMemberId();
+			//DateTime attendDateTime = DateTime.ParseExact(attendDate, "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal);
+			var result = _memberService.FollowActivity(memberId, activityId, attendDate);
+
+			if (!result.Success)
+			{
+				return BadRequest(result.Message);
+			}
+
+			return Ok(result.Message);
+		}
+
+		[HttpDelete]
+		[Route("Activities/{activityId}")]
+		public IActionResult UnfollowActivity(int activityId)
+		{
+			int memberId = this.GetMemberId();
+			var result = _memberService.UnfollowActivity(memberId, activityId);
+
+			if (!result.Success)
+			{
+				return BadRequest(result.Message);
+			}
+
+			return Ok(result.Message);
+		}
+	}
+}
---
+++ Models/Services/MemberService.cs
@@ -0,0 +1,68 @@
+﻿using api.iSMusic.Models.DTOs.ActivityDTOs;
+using api.iSMusic.Models.Services.Interfaces;
+
+namespace api.iSMusic.Models.Services
+{
+	public class MemberService
+	{
+		private readonly IMemberRepository _memberRepository;
+
+		
+		private readonly IActivityRepository _activityRepository;
+
+
+        public MemberService(IMemberRepository repo,  IActivityRepository activityRepository)
+		{
+			_memberRepository = repo;
+			_activityRepository = activityRepository;
+
+        }
+
+		
+        public (bool Success, string Message, IEnumerable<ActivityIndexDTO>Dtos) GetMemberFollowedActivities(int memberId)
+		{
+			if (CheckMemberExistence(memberId) == false) return (false, "會員不存在", new List<ActivityIndexDTO>());
+
+			var dtos = _activityRepository.GetMemberFollowedActivities(memberId);
+			return (true, string.Empty, dtos);
+        }
+
+
+        public (bool Success, string Message) FollowActivity(int memberId, int activityId, DateTime attendDate)
+		{
+			if (CheckMemberExistence(memberId) == false) return (false, "會員不存在");
+			if(CheckActivityExistence(activityId) == false) return (false, "活動不存在");
+
+            _activityRepository.FollowActivity(memberId, activityId, attendDate);
+            return (true, "成功新增");
+        }
+ 
+
+        public (bool Success, string Message) UnfollowActivity(int memberId, int activityId)
+        {
+            if (CheckMemberExistence(memberId) == false) return (false, "會員不存在");
+
+            if (CheckActivityExistence(activityId) == false) return (false, "活動不存在");
+
+            _activityRepository.UnfollowActivity(memberId, activityId);
+            return (true, "成功刪除");
+        }
+
+      
+
+		private bool CheckMemberExistence(int memberId)
+		{
+			var member = _memberRepository.GetMemberById(memberId);
+
+			return member != null;
+		}
+
+        private bool CheckActivityExistence(int activityId)
+        {
+            var activity = _activityRepository.GetActivityByIdForCheck(activityId);
+
+            return activity != null;
+        }
+
+    }
+}
